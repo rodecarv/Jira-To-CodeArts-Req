80b4c88abae90ee10944e3bf2678809edea52ef4
Matheus Farias de Oliveira Matsumoto <m50037921@china.huawei.com>
2024-01-12 14:15:42 -0300

    Created more functions for the Templates class

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import csv
 header_codearts = [] # Array.
 data_codearts = [] # Matrix.
 
-class JiraTemplate():
+class JiraTemplate:
     def __init__(self, jira_file:str):
         self.header = []
         self.data_jira = {}
@@ -36,33 +36,63 @@ class JiraTemplate():
             for i in data:
                 current_line = {}
                 for index, head in enumerate(self.header): 
-                    #print(f'{index}: Data[{data.index(i)}] : {head} : {i[index]}')
                     current_line.update({head : i[index]})
                 self.data_jira[data.index(i)] = current_line
-                    
+    
+    def show_pretty_data(self):
+        for i in jira.data_jira:
+            print(f'\n{i}')
+            # j == header
+            for j in jira.data_jira[i]:
+                if jira.data_jira[i][j] != '':
+                    print(f'   {j} : {jira.data_jira[i][j]}')
 
-# Ler o arquivo csv do codearts e guardar os dados em data_codearts.
-def read_codearts_csv(file_name:str):
 
-    with open(file_name, mode='r') as file:
-        csv_file = csv.reader(file)
-        for i, lines in enumerate(csv_file):
-            
-            # Coletar o header.
-            if i == 0:
-                for e in lines:
-                    header_codearts.append(e)
+class CodeArtsTemplate:
+    def __init__(self, codearts_file:str):
+        self.header = []
+        self.data_codearts = {}
+        self.codearts_file = codearts_file
 
-            # Coletar cada dado.
-            else:
-                data_codearts.append(lines)
+    def __repr__(self):
+        return f"""JiraTemplate(
+            header = {self.header} ;
+            data_codearts = {self.data_jira} ;
+            codearts_file = {self.codearts_file}
+        )"""
 
-        #print('\t ------ CODEARTS!!!! ------\n')
-        #print(f'\nHeader do CodeArts: {header_codearts}\n')
-        for i in data_codearts:
-            for index, head in enumerate(header_codearts): 
-                print(f'{index}: Data[{data_codearts.index(i)}] : {head} : {i[index]}')
+    def read_codearts_csv(self):
+        with open(self.codearts_file, mode='r') as file:
+            csv_file = csv.reader(file)
+            data = []
+            for i, lines in enumerate(csv_file):
+                # Coletar o header.
+                if i == 0:
+                    for e in lines:
+                        self.header.append(e)
+                # Coletar cada dado.
+                else:
+                    data.append(lines)
 
+            for i in data:
+                current_line = {}
+                for index, head in enumerate(self.header): 
+                    current_line.update({head : i[index]})
+                self.data_codearts[data.index(i)] = current_line
+
+    def show_pretty_data():
+        for i in codearts.data_codearts:
+            print(f'\n{i}')
+            # j == header
+            for j in codearts.data_codearts[i]:
+                if codearts.data_codearts[i][j] != '':
+                    print(f'   {j} : {codearts.data_codearts[i][j]}')
+
+
+class TemplateController:
+    def __init__(self, codearts:CodeArtsTemplate, jira:JiraTemplate):
+        self.codearts = codearts
+        self.jira = jira
 
 def new_table(file_name:str):
     # with open(file_name, mode='w') as file:
@@ -75,11 +105,13 @@ if __name__ == '__main__':
     jira = JiraTemplate(jira_file='jira.csv')
     jira.read_jira_csv()
     
+    codearts = CodeArtsTemplate(codearts_file='codearts.csv')
+    codearts.read_codearts_csv()
+
+    print('\nJIRA:')
     # i == chave dos dicionarios dentro do dicionario.
-    for i in jira.data_jira:
-        print(i)
-        # j == header
-        for j in jira.data_jira[i]:
-            if jira.data_jira[i][j] != '':
-                print(f'   {j} : {jira.data_jira[i][j]}')
+    jira.show_pretty_data()
+
+    print('\nCODEARTS:')
+    codearts.show_pretty_data()
         
